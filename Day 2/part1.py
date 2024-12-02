22adbc9002d27578759f306f9d37ffd327fd0f32
kadle <kadle@email.com>
2024-12-02 20:24:41 +0530

    New Code

--- Day 2/part1.py
+++ Day 2/part1.py
@@ -1,19 +1,27 @@
-import csv 
+import csv
 
-unsafe_count = 0
-with open('reports.csv') as f:
-    r = csv.reader(f)
-    for row in r:
-        if row == sorted(row) or row == sorted(row, reverse=True):
-            c = 0
-            for i in range(1, len(row)):
-                diff = abs(int(row[i])-int(row[i-1]))
-                while c == 0:
-                    if diff > 3 or diff == 0:
-                        c += 1
-                        unsafe_count += 1
-                    break
-        else:
-            unsafe_count += 1
+def main():
+    with open('D:/Advent Of Code/Day 2/reports.csv') as f:
+        r = csv.reader(f)
+        safe_count = 0
+        for row in r:
+            if isAscendingOrDescending(row) == False or checkSafe(row) == False:
+                continue
+            else:
+                safe_count += 1
+        print(safe_count)
 
-print(unsafe_count)
+
+def isAscendingOrDescending(input):
+    if input != sorted(input) and input != sorted(input, reverse=True):
+        return False
+
+
+def checkSafe(input):
+    for index in range(1, len(input)):
+        if abs(int(input[index])-int(input[index-1])) == 0 or abs(int(input[index])-int(input[index-1])) > 3:
+            return False
+
+
+if __name__ == "__main__":
+    main()
